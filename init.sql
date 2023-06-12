6bc3f845e4d1c7c64b16cc07fb225576ca1463b2
Yngve Molnes <yngve.molnes@gmail.com>
2023-06-12 22:05:02 +0200

    Normalize data model

--- init.sql
+++ init.sql
@@ -1,79 +1,93 @@
--- Table: Customer
-CREATE TABLE IF NOT EXISTS Customer (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  ad_group_id INT
-);
-
--- Table: ADGroup
-CREATE TABLE IF NOT EXISTS ADGroup (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS customer (
+  customer_id SERIAL PRIMARY KEY,
   name TEXT NOT NULL
 );
 
--- Table: Solution
-CREATE TABLE IF NOT EXISTS Solution (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS ad_group (
+  ad_group_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS solution (
+  solution_id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   customer_id INT
 );
 
--- Table: Component
-CREATE TABLE IF NOT EXISTS Component (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS component (
+  component_id SERIAL PRIMARY KEY,
   name TEXT NOT NULL
 );
 
--- Table: SolutionComponent
-CREATE TABLE IF NOT EXISTS SolutionComponent (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS solution_component (
   solution_id INT,
-  component_id INT
+  component_id INT,
+  PRIMARY KEY (solution_id, component_id)
 );
 
--- Table: Environment
-CREATE TABLE IF NOT EXISTS Environment (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS environment (
+  environment_id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
-  solution_id INT,
-  vm_id INT,
+  solution_id INT
+);
+
+CREATE TABLE IF NOT EXISTS virtual_machine (
+  virtual_machine_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  specifications JSONB
+);
+
+CREATE TABLE IF NOT EXISTS dedicated_cluster (
+  dedicated_cluster_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  specifications JSONB
+);
+
+CREATE TABLE IF NOT EXISTS shared_cluster (
+  shared_cluster_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  specifications JSONB
+);
+
+CREATE TABLE IF NOT EXISTS environment_virtual_machine (
+  environment_id INT,
+  virtual_machine_id INT,
+  PRIMARY KEY (environment_id, virtual_machine_id)
+);
+
+CREATE TABLE IF NOT EXISTS environment_dedicated_cluster (
+  environment_id INT,
   dedicated_cluster_id INT,
-  shared_cluster_id INT
+  PRIMARY KEY (environment_id, dedicated_cluster_id)
 );
 
--- Table: VirtualMachine
-CREATE TABLE IF NOT EXISTS VirtualMachine (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  specifications JSON
+CREATE TABLE IF NOT EXISTS environment_shared_cluster (
+  environment_id INT,
+  shared_cluster_id INT,
+  PRIMARY KEY (environment_id, shared_cluster_id)
 );
 
--- Table: DedicatedCluster
-CREATE TABLE IF NOT EXISTS DedicatedCluster (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  specifications JSON
-);
-
--- Table: SharedCluster
-CREATE TABLE IF NOT EXISTS SharedCluster (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  specifications JSON
-);
-
--- Table: Namespace
-CREATE TABLE IF NOT EXISTS Namespace (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS namespace (
+  namespace_id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   environment_id INT
 );
 
--- Add foreign key constraints
-ALTER TABLE Customer ADD CONSTRAINT fk_customer_adgroup FOREIGN KEY (ad_group_id) REFERENCES ADGroup(id);
-ALTER TABLE Solution ADD CONSTRAINT fk_solution_customer FOREIGN KEY (customer_id) REFERENCES Customer(id);
-ALTER TABLE SolutionComponent ADD CONSTRAINT fk_solutioncomponent_solution FOREIGN KEY (solution_id) REFERENCES Solution(id);
-ALTER TABLE SolutionComponent ADD CONSTRAINT fk_solutioncomponent_component FOREIGN KEY (component_id) REFERENCES Component(id);
-ALTER TABLE Environment ADD CONSTRAINT fk_environment_solution FOREIGN KEY (solution_id) REFERENCES Solution(id);
-ALTER TABLE Namespace ADD CONSTRAINT fk_namespace_environment FOREIGN KEY (environment_id) REFERENCES Environment(id);
-
+ALTER TABLE customer DROP CONSTRAINT IF EXISTS fk_customer_ad_group;
+ALTER TABLE solution DROP CONSTRAINT IF EXISTS fk_solution_customer;
+ALTER TABLE solution_component DROP CONSTRAINT IF EXISTS fk_solution_component_solution;
+ALTER TABLE solution_component DROP CONSTRAINT IF EXISTS fk_solution_component_component;
+ALTER TABLE environment DROP CONSTRAINT IF EXISTS fk_environment_solution;
+ALTER TABLE namespace DROP CONSTRAINT IF EXISTS fk_namespace_environment;
+ALTER TABLE customer ADD CONSTRAINT fk_customer_ad_group FOREIGN KEY (ad_group_id) REFERENCES ad_group(ad_group_id);
+ALTER TABLE solution ADD CONSTRAINT fk_solution_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id);
+ALTER TABLE solution_component ADD CONSTRAINT fk_solution_component_solution FOREIGN KEY (solution_id) REFERENCES solution(solution_id);
+ALTER TABLE solution_component ADD CONSTRAINT fk_solution_component_component FOREIGN KEY (component_id) REFERENCES component(component_id);
+ALTER TABLE environment ADD CONSTRAINT fk_environment_solution FOREIGN KEY (solution_id) REFERENCES solution(solution_id);
+ALTER TABLE namespace ADD CONSTRAINT fk_namespace_environment FOREIGN KEY (environment_id) REFERENCES environment(environment_id);
+ALTER TABLE environment_virtual_machine ADD CONSTRAINT fk_environment_virtual_machine_environment FOREIGN KEY (environment_id) REFERENCES environment(environment_id);
+ALTER TABLE environment_virtual_machine ADD CONSTRAINT fk_environment_virtual_machine_virtual_machine FOREIGN KEY (virtual_machine_id) REFERENCES virtual_machine(virtual_machine_id);
+ALTER TABLE environment_dedicated_cluster ADD CONSTRAINT fk_environment_dedicated_cluster_environment FOREIGN KEY (environment_id) REFERENCES environment(environment_id);
+ALTER TABLE environment_dedicated_cluster ADD CONSTRAINT fk_environment_dedicated_cluster_dedicated_cluster FOREIGN KEY (dedicated_cluster_id) REFERENCES dedicated_cluster(dedicated_cluster_id);
+ALTER TABLE environment_shared_cluster ADD CONSTRAINT fk_environment_shared_cluster_environment FOREIGN KEY (environment_id) REFERENCES environment(environment_id);
+ALTER TABLE environment_shared_cluster ADD CONSTRAINT fk_environment_shared_cluster_shared_cluster FOREIGN KEY (shared_cluster_id) REFERENCES shared_cluster(shared_cluster_id);
